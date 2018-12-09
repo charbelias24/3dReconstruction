9fe9c409bd1df2a849d5148eccc9d4045dc918f1
Charbel <charbelias.1998@gmail.com>
2018-12-10 00:15:25 +0200

    Fixed address reusage. Recieving and sending images is complete.

--- server_test2.py
+++ server_test2.py
@@ -1,6 +1,7 @@
 import socket
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server_socket.bind(("22.22.22.22", 5002))
 server_socket.listen(5)
 

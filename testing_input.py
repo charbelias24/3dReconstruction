590b62ec369e14a9dade5503e53f8d74d3c02b82
Charbel <charbelias.1998@gmail.com>
2018-12-11 20:32:52 +0200

    Testing Button

--- testing_input.py
+++ testing_input.py
@@ -1,5 +1,3 @@
-# This is for the master(server) RPi
-
 import cv2
 import time
 import os
@@ -13,9 +11,10 @@ GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 def main():
     while True:
         if GPIO.input(button):
-            os.system("/usr/bin/python server_test3.py")
-            break
-        time.sleep(0.01)
+            print ("ON")
+        else:
+            print ("OFF")
+        time.sleep(0.5)
 
 if __name__=='__main__':
     main()
